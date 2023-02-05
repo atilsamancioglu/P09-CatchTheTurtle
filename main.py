2d3e8fba9f4e3b76c6f59b376a5a607ff9f10318
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2023-02-05 13:41:01 +0300

    catch the turtle completed

---
+++ main.py
@@ -0,0 +1,127 @@
+import random
+import turtle
+
+screen = turtle.Screen()
+game_over = False
+score = 0
+FONT = ('Arial', 30, 'normal')
+
+#turtle list
+turtle_list = []
+
+#countdown turtle
+count_down_turtle = turtle.Turtle()
+
+# score turtle
+score_turtle = turtle.Turtle()
+
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color("blue")
+    score_turtle.penup()
+
+    top_height = screen.window_height() / 2  # positive height/2 is the top of the screen
+    y = top_height - top_height / 10  # decreasing a bit so text will be visible
+    score_turtle.setposition(0, y)
+    score_turtle.write(arg='Score: 0', move=False, align='center', font=FONT)
+
+grid_size = 10
+
+def make_turtle(x, y):
+    t = turtle.Turtle()
+
+    def handle_click(x, y):
+        global score
+        score += + 1
+        score_turtle.clear()
+        score_turtle.write("Score: {}".format(score), move=False, align="center", font=FONT)
+        print(x, y)
+
+    t.onclick(handle_click)
+    t.penup()
+    t.shape("turtle")
+    t.shapesize(2, 2)
+    t.color("green")
+    t.goto(x * grid_size, y * grid_size)
+    t.pendown()
+    turtle_list.append(t)
+
+
+x_coordinates = [-20, -10, 0, 10, 20]
+y_coordinates = [20, 10, 0, -10]
+
+def setup_turtles():
+    for x in x_coordinates:
+        for y in y_coordinates:
+            make_turtle(x, y)
+    '''
+    make_turtle(-20,20)
+    make_turtle(-10,20)
+    make_turtle(-0,20)
+    make_turtle(10,20)
+    make_turtle(20,20)
+    make_turtle(-20,10)
+    make_turtle(-10,10)
+    make_turtle(-0,10)
+    make_turtle(10,10)
+    make_turtle(20,10)
+    make_turtle(-20,0)
+    make_turtle(-10,0)
+    make_turtle(-0,0)
+    make_turtle(10,0)
+    make_turtle(20,0)
+    make_turtle(-20,-10)
+    make_turtle(-10,-10)
+    make_turtle(-0,-10)
+    make_turtle(10,-10)
+    make_turtle(20,-10)
+    '''
+
+def hide_turtles():
+    for t in turtle_list:
+        t.hideturtle()
+
+def show_turtles_randomly():
+    if not game_over:
+        hide_turtles()
+        random.choice(turtle_list).showturtle()
+        screen.ontimer(show_turtles_randomly, 500)
+
+
+def countdown(time):
+    global game_over
+    top_height = screen.window_height() / 2
+    y = top_height - top_height / 10
+    count_down_turtle.hideturtle()
+    count_down_turtle.penup()
+    count_down_turtle.setposition(0, y - 30)
+    count_down_turtle.clear()
+
+    if time > 0:
+        count_down_turtle.clear()
+        count_down_turtle.write("Time: {}".format(time),move=False,align="center",font=FONT)
+        screen.ontimer(lambda: countdown(time - 1), 1000)
+    else:
+        game_over = True
+        count_down_turtle.clear()
+        hide_turtles()
+        count_down_turtle.write("Game Over!", align='center', font=FONT)
+        restart = screen.textinput("Game over!","\nDo you want to restart ? (y/n)")
+        if restart == "y":
+            screen.clearscreen()
+            start_game_up()
+
+
+def start_game_up():
+    global game_over
+    game_over = False
+    turtle.tracer(0)
+    setup_score_turtle()
+    setup_turtles()
+    hide_turtles()
+    show_turtles_randomly()
+    turtle.tracer(1)
+    screen.ontimer(lambda: countdown(10), 10)
+
+start_game_up()
+turtle.mainloop()
